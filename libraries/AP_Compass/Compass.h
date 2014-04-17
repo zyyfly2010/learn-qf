a8f7bd1990898feaa66d6852554cde811f453c48
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2014-04-17 13:37:23 +0200

    HAL_BOARD_ERLE: new abstraction layer, Erle Robot
    A layer for the OS/OW Erle Robot quadcopter has been
    created. For now the robot relies on the AP_HAL_LINUX
    layer.

--- libraries/AP_Compass/Compass.h
+++ libraries/AP_Compass/Compass.h
@@ -36,6 +36,8 @@
 # define MAG_BOARD_ORIENTATION ROTATION_YAW_90
 #elif CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
 # define MAG_BOARD_ORIENTATION ROTATION_NONE
+#elif CONFIG_HAL_BOARD == HAL_BOARD_ERLE
+# define MAG_BOARD_ORIENTATION ROTATION_YAW_90
 #else
 # error "You must define a default compass orientation for this board"
 #endif
--- libraries/AP_Notify/AP_BoardLED.h
+++ libraries/AP_Notify/AP_BoardLED.h
@@ -66,6 +66,12 @@
  # define HAL_GPIO_C_LED_PIN        25
  # define HAL_GPIO_LED_ON           LOW
  # define HAL_GPIO_LED_OFF          HIGH
+#elif CONFIG_HAL_BOARD == HAL_BOARD_ERLE
+ # define HAL_GPIO_A_LED_PIN        -1
+ # define HAL_GPIO_B_LED_PIN        -1
+ # define HAL_GPIO_C_LED_PIN        -1
+ # define HAL_GPIO_LED_ON           LOW
+ # define HAL_GPIO_LED_OFF          HIGH
 #else
 #error "Unknown board type in AP_Notify"
 #endif
