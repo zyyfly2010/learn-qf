f2dcb2c3821ea42aedddaecc544303e173a6f670
Andrew Tridgell <andrew@tridgell.net>
2015-03-05 15:06:11 +1100

    REFERENCE: allow servo set on all channels

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -121,7 +121,7 @@ void Plane::init_ardupilot()
     serial_manager.init();
 
     // allow servo set on all channels except first 4
-    ServoRelayEvents.set_channel_mask(0xFFF0);
+    ServoRelayEvents.set_channel_mask(0xFFFF);
 
     set_control_channels();
 
