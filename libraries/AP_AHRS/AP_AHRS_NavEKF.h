b32b4c68fc14fc7913c51fe8568e104e61ca1a32
Jonathan Challinger <mr.challinger@gmail.com>
2015-04-27 12:42:07 -0700

    AP_AHRS_NavEKF: add setTakeoffExpected, setTouchdownExpected

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -114,6 +114,11 @@ AP_AHRS_NavEKF(AP_InertialSensor &ins, AP_Baro &baro, AP_GPS &gps, RangeFinder &
 
     void set_ekf_use(bool setting);
 
+
+    void setTakeoffExpected(bool val);
+    void setTouchdownExpected(bool val);
+
+
     // is the AHRS subsystem healthy?
     bool healthy(void) const;
 
