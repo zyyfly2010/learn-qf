3b19c22496e027dc31ebbd541489ebde53a4386c
ali <alirezakhosravian@gmail.com>
2015-05-22 13:45:35 +1000

    AP_NavEKF2 and AP_Predictors: IMU reading has been replaced by store and Bestindex functions.  Some cleaning up in AP_NavEKF2 and AP_Predictors

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -900,98 +900,53 @@ void NavEKF2::UpdateStrapdownEquationsNED()
     Quaternion deltaQuat; // quaternion from last to current time step
     const Vector3f gravityNED(0, 0, GRAVITY_MSS); // NED gravity vector m/s^2
 
-// sean
+
     Matrix3f Tbn_temp;
-    Vector3f D_q_tmp;  // sean   temporary vector part of quaternion Delta
-    Quaternion q_tmp;  // sean   temporary quaternion
-    Quaternion q_tmp_m;  // sean   temporary quaternion
+
+    uint16_t tmp_index;
+    tmp_index=storeIndexIMU;
+    storeDataVector(dAngIMU, storedAngRate, lastAngRateStoreTime_ms, angRateTimeStamp, storeIndexIMU, imuSampleTime_ms);
+    // Check if new Gyro data is stored. If yes, then store also ACCEL data.
+    if (tmp_index != storeIndexIMU){
+        storeddVelIMU1[storeIndexIMU-1] = dVelIMU1;
+        storeddVelIMU2[storeIndexIMU-1] = dVelIMU2;
+     }
 
 
-    if (init_reset==0) {
-        D_q[0]=1;
-        D_q[1]=0;
-        D_q[2]=0;
-        D_q[3]=0;
+//    if (imuSampleTime_ms - lastAngRateStoreTime_ms >= 10) {
+//        lastAngRateStoreTime_ms = imuSampleTime_ms;
+//        if (storeIndexIMU > (BUFFER_SIZE-1)) {
+//            storeIndexIMU = 0;
+//        }
+//        storedAngRate[storeIndexIMU] = dAngIMU;
+//        storeddVelIMU1[storeIndexIMU] = dVelIMU1;
+//        storeddVelIMU2[storeIndexIMU] = dVelIMU2;
+//        angRateTimeStamp[storeIndexIMU] = lastAngRateStoreTime_ms;
+//        storeIndexIMU = storeIndexIMU + 1;
+//    }
 
-        D_q_k1[0]=1;
-        D_q_k1[1]=0;
-        D_q_k1[2]=0;
-        D_q_k1[3]=0;
-
-        q_hat[0]=1;
-        q_hat[1]=0;
-        q_hat[2]=0;
-        q_hat[3]=0;
-
-        q_hat_T_k1[0]=1;
-        q_hat_T_k1[1]=0;
-        q_hat_T_k1[2]=0;
-        q_hat_T_k1[3]=0;
-
-        delta_q[0]=1;
-        delta_q[1]=0;
-        delta_q[2]=0;
-        delta_q[3]=0;
-
-
-        D_q_tmp[0]=0;
-        D_q_tmp[1]=0;
-        D_q_tmp[2]=0;
-        for (uint16_t i=0; i<=(BUFFER_SIZE-1); i++) {
-            storedD_s[i]=1;
-            storedD_v[i] =D_q_tmp;
-//		printf("%u\n",i);
-        }
-        init_reset=1;
-    }
-//lastAngRateStoreTime_ms=1;
-//storeIndexIMU=1;
-// cin >> angRateTimeStamp[5];
-//cout << angRateTimeStamp[5] << "\n";
-//cout << lastAngRateStoreTime_ms << "\n";
-//printf("%u \n", storeIndexIMU);
-
-    if (imuSampleTime_ms - lastAngRateStoreTime_ms >= 10) {
-        lastAngRateStoreTime_ms = imuSampleTime_ms;
-        if (storeIndexIMU > (BUFFER_SIZE-1)) {
-            storeIndexIMU = 0;
-        }
-        storedAngRate[storeIndexIMU] = dAngIMU;
-        storeddVelIMU1[storeIndexIMU] = dVelIMU1;
-        storeddVelIMU2[storeIndexIMU] = dVelIMU2;
-        angRateTimeStamp[storeIndexIMU] = lastAngRateStoreTime_ms;
-        storeIndexIMU = storeIndexIMU + 1;
-
-//printf("%u , %u \n", storeIndexIMU, angRateTimeStamp[storeIndexIMU]);
-//        cout << imuSampleTime_ms << "   " << storedAngRate[storeIndexIMU] << "\n";
-    }
-
-//    int GPS_delay_AngRate = ;
     uint32_t timeDeltaAngRate;
-    uint32_t bestTimeDeltaAngRate = 2000;
-    uint16_t bestStoreIndex = 0;
-//    state_elements state_delay;
+    AP_Int16 tempZeroTime;
+    uint32_t bestTimeDelta = MAX_MSDELAY;
+    uint16_t bestStoreIndex;
     Vector3f dAngIMU_Delay;
     Vector3f dVelIMU1_Delay;
     Vector3f dVelIMU2_Delay;
-//     Vector3f gyro_bias_delay;
 
-    for (uint16_t i=0; i<=(BUFFER_SIZE-1); i++)
-    {
-        timeDeltaAngRate = abs( (imuSampleTime_ms - angRateTimeStamp[i]) - constrain_int16(_msecEkfDelay, 0, MAX_MSDELAY));
-        if (timeDeltaAngRate < bestTimeDeltaAngRate)
-        {
-            bestStoreIndex = i;
-            bestTimeDeltaAngRate = timeDeltaAngRate;
-        }
-    }
+    BestIndex(bestTimeDelta, bestStoreIndex, angRateTimeStamp, _msecEkfDelay, tempZeroTime);
+//
+//    for (uint16_t i=0; i<=(BUFFER_SIZE-1); i++)
+//    {
+//        timeDeltaAngRate = abs( (imuSampleTime_ms - angRateTimeStamp[i]) - constrain_int16(_msecEkfDelay, 0, MAX_MSDELAY));
+//        if (timeDeltaAngRate < bestTimeDeltaAngRate)
+//        {
+//            bestStoreIndex = i;
+//            bestTimeDeltaAngRate = timeDeltaAngRate;
+//        }
+//    }
     dAngIMU_Delay = storedAngRate[bestStoreIndex];
     dVelIMU1_Delay=storeddVelIMU1[bestStoreIndex];
     dVelIMU2_Delay=storeddVelIMU2[bestStoreIndex];
-
-// printf("%u, %u , %u, %u, %u , %d \n", imuSampleTime_ms, storeIndexIMU, bestStoreIndex, bestTimeDeltaAngRate, angRateTimeStamp[storeIndexIMU-1], int(angRateTimeStamp[storeIndexIMU-1]-angRateTimeStamp[bestStoreIndex]));
-
-
 
     correctedDelAng = dAngIMU_Delay - state.gyro_bias;
     correctedDelVel1 = dVelIMU1_Delay;
@@ -1095,9 +1050,7 @@ void NavEKF2::UpdateStrapdownEquationsNED()
 
 
     /////////////////////////// Sean: (12 - 15)/12/2014 -- Predictor ///////////////////////////////////////////////////////////
-// retreive previous calculations
-// D_T = D;
-
+    Vector3f tilde_q;
     tilde_q = dAngIMU - state.gyro_bias;
 
     corrected_tilde_Vel1 = dVelIMU1;
@@ -1105,273 +1058,10 @@ void NavEKF2::UpdateStrapdownEquationsNED()
     corrected_tilde_Vel1.z -= state.accel_zbias1;
     corrected_tilde_Vel2.z -= state.accel_zbias2;
     corrected_tilde_Vel12 =  corrected_tilde_Vel1* IMU1_weighting +  corrected_tilde_Vel2* (1.0f - IMU1_weighting);
-//    tilde_Vel  = Tbn_temp*corrected_tilde_Vel12 + gravityNED*dtIMU;
 
-
-//test_Predictor.AttitudePredictor(dAngIMU, state.gyro_bias, _msecPosDelay, state.quat);
-//test_Predictor.VelocityPredictor(state.quat, dVelIMU1, dVelIMU2, IMU1_weighting, dtIMU, _msecPosDelay, state.accel_zbias1, state.accel_zbias2, state.velocity, state.position);
 // uint16_t test_delay=_msecEkfDelay+20;
     test_Predictor.CascadedPredictor(tilde_q, corrected_tilde_Vel12, state.quat, dtIMU, imuSampleTime_ms, _msecEkfDelay, state.velocity, state.position);
 
-
-
-
-//
-//D_q = D_q_k1;
-//// q_hat = q_hat_T_k1;
-//
-///*
-//// calculate Delta matrix from quaternion form.
-//D[0][0] = D_q[1]*D_q[1]+D_q[2]*D_q[2]-D_q[3]*D_q[3]-D_q[4]*D_q[4];
-//D[0][1] = 2.0f*D_q[2]*D_q[3]-2.0f*D_q[1]*D_q[4];
-//D[0][2] = 2.0f*D_q[1]*D_q[3]+2.0f*D_q[2]*D_q[4];
-//D[1][0] = 2.0f*D_q[2]*D_q[3] + 2.0f*D_q[1]*D_q[4];
-//D[1][1] = D_q[1]*D_q[1]-D_q[2]*D_q[2]+D_q[3]*D_q[3]-D_q[4]*D_q[4];
-//D[1][2] = -2.0f*D_q[1]*D_q[2]+2.0f*D_q[3]*D_q[4];
-//D[2][0] = -2.0f*D_q[1]*D_q[3] + 2.0f*D_q[2]*D_q[4];
-//D[2][1] = 2.0f*D_q[1]*D_q[2] + 2.0f*D_q[3]*D_q[4];
-//D[2][2] = D_q[1]*D_q[1]-D_q[2]*D_q[2]-D_q[3]*D_q[3]+D_q[4]*D_q[4];
-//*/
-//
-//tilde_q = dAngIMU - state.gyro_bias;
-//
-//n_tilde_q = sqrt(tilde_q[0]*tilde_q[0]+tilde_q[1]*tilde_q[1]+tilde_q[2]*tilde_q[2]);
-//if (n_tilde_q < 1e-12f)
-//{
-//    delta_q[0] = 1;
-//    delta_q[1] = 0;
-//    delta_q[2] = 0;
-//    delta_q[3] = 0;
-//}
-//else
-//{
-//// check: delta_q=single([cos(0.5*n_tilde_q);(sin(0.5*n_tilde_q)/n_tilde_q).*tilde_q]);
-//    delta_q[0] = cosf(0.5f * n_tilde_q);
-//    rotScaler = (sinf(0.5f * n_tilde_q)) / n_tilde_q;
-//    delta_q[1] = tilde_q.x * rotScaler;
-//    delta_q[2] = tilde_q.y * rotScaler;
-//    delta_q[3] = tilde_q.z * rotScaler;
-//}
-//
-//// update the quaternions by rotating from the previous attitude through
-//// the delta angle rotation quaternion
-//D_q_k1[0] = D_q[0]*delta_q[0] - D_q[1]*delta_q[1] - D_q[2]*delta_q[2] - D_q[3]*delta_q[3];
-//D_q_k1[1] = D_q[0]*delta_q[1] + D_q[1]*delta_q[0] + D_q[2]*delta_q[3] - D_q[3]*delta_q[2];
-//D_q_k1[2] = D_q[0]*delta_q[2] + D_q[2]*delta_q[0] + D_q[3]*delta_q[1] - D_q[1]*delta_q[3];
-//D_q_k1[3] = D_q[0]*delta_q[3] + D_q[3]*delta_q[0] + D_q[1]*delta_q[2] - D_q[2]*delta_q[1];
-//
-//
-//n_D_q_k1 = D_q_k1[0]*D_q_k1[0]+D_q_k1[1]*D_q_k1[1]+D_q_k1[2]*D_q_k1[2]+D_q_k1[3]*D_q_k1[3];
-//D_q_k1[0] = D_q_k1[0]/n_D_q_k1;
-//D_q_k1[1] = D_q_k1[1]/n_D_q_k1;
-//D_q_k1[2] = D_q_k1[2]/n_D_q_k1;
-//D_q_k1[3] = D_q_k1[3]/n_D_q_k1;
-//
-//
-//
-//    if (imuSampleTime_ms - lastDStoreTime_ms >= 10) {
-//        lastDStoreTime_ms = imuSampleTime_ms;
-//        if (storeIndexD > (BUFFER_SIZE-1)) {
-//            storeIndexD = 0;
-//        }
-//        storedD_s[storeIndexD]=D_q_k1[0];
-//        D_q_tmp[0]=D_q_k1[1];
-//        D_q_tmp[1]=D_q_k1[2];
-//        D_q_tmp[2]=D_q_k1[3];
-//        storedD_v[storeIndexD] = D_q_tmp;
-//        DTimeStamp[storeIndexD] = lastDStoreTime_ms;
-//        storeIndexD = storeIndexD + 1;
-//
-////printf("%u , %u \n", storeIndexIMU, angRateTimeStamp[storeIndexIMU]);
-////        cout << imuSampleTime_ms << "   " << storedAngRate[storeIndexIMU] << "\n";
-//    }
-//
-//
-//
-//    int timeD;
-//    uint32_t bestTimeD = 200;
-//    Quaternion D_Delay;
-//    bestStoreIndex = 0;
-//
-//    for (uint16_t i=0; i<=(BUFFER_SIZE-1); i++)
-//    {
-//        timeD = abs( (int) imuSampleTime_ms- DTimeStamp[i] - constrain_int16(_msecPosDelay, 0, MAX_MSDELAY));
-//        //       printf("%u \n",_msecPosDelay);
-//	if (timeD < bestTimeD)
-//        {
-//            bestStoreIndex = i;
-//            bestTimeD = timeD;
-//        }
-//    }
-//
-//D_Delay[0] = storedD_s[bestStoreIndex];
-//D_q_tmp=storedD_v[bestStoreIndex];
-//D_Delay[1] = D_q_tmp[0];
-//D_Delay[2] = D_q_tmp[1];
-//D_Delay[3] = D_q_tmp[2];
-//
-//// D_q_delay^{-1}
-//q_tmp[0]= D_Delay[0];
-//q_tmp[1]= -D_Delay[1];
-//q_tmp[2]= -D_Delay[2];
-//q_tmp[3]= -D_Delay[3];
-//
-//// D_q_delay^{-1} \times D_q
-//
-//q_hat[0] = q_tmp[0]*D_q[0] - q_tmp[1]*D_q[1] - q_tmp[2]*D_q[2] - q_tmp[3]*D_q[3];
-//q_hat[1] = q_tmp[0]*D_q[1] + q_tmp[1]*D_q[0] + q_tmp[2]*D_q[3] - q_tmp[3]*D_q[2];
-//q_hat[2] = q_tmp[0]*D_q[2] + q_tmp[2]*D_q[0] + q_tmp[3]*D_q[1] - q_tmp[1]*D_q[3];
-//q_hat[3] = q_tmp[0]*D_q[3] + q_tmp[3]*D_q[0] + q_tmp[1]*D_q[2] - q_tmp[2]*D_q[1];
-//
-//// q_hat_tau \times D_q_delay^{-1} \times D_q
-//
-//q_tmp[0] = state.quat[0]*q_hat[0] - state.quat[1]*q_hat[1] - state.quat[2]*q_hat[2] - state.quat[3]*q_hat[3];
-//q_tmp[1] = state.quat[0]*q_hat[1] + state.quat[1]*q_hat[0] + state.quat[2]*q_hat[3] - state.quat[3]*q_hat[2];
-//q_tmp[2] = state.quat[0]*q_hat[2] + state.quat[2]*q_hat[0] + state.quat[3]*q_hat[1] - state.quat[1]*q_hat[3];
-//q_tmp[3] = state.quat[0]*q_hat[3] + state.quat[3]*q_hat[0] + state.quat[1]*q_hat[2] - state.quat[2]*q_hat[1];
-//
-//q_tmp.normalize();
-//q_hat=q_tmp;
-//
-//
-//// velocity prediction
-//    Matrix3f prevTnb_pred;
-//
-//    q_hat.rotation_matrix(Tbn_temp);
-//    prevTnb_pred = Tbn_temp.transposed();
-//
-//    Vector3f tilde_Vel;
-//    Vector3f corrected_tilde_Vel1;
-//    Vector3f corrected_tilde_Vel2;
-//    Vector3f corrected_tilde_Vel12;
-//
-//    corrected_tilde_Vel1 = dVelIMU1;
-//    corrected_tilde_Vel2 = dVelIMU2;
-//    corrected_tilde_Vel1.z -= state.accel_zbias1;
-//    corrected_tilde_Vel2.z -= state.accel_zbias2;
-//    corrected_tilde_Vel12 =  corrected_tilde_Vel1* IMU1_weighting +  corrected_tilde_Vel2* (1.0f - IMU1_weighting);
-//    tilde_Vel  = Tbn_temp*corrected_tilde_Vel12 + gravityNED*dtIMU;
-//
-//    d_v+=tilde_Vel ;
-//
-//// buffering d_v
-//        storeIndexD = storeIndexD - 1;
-//	storedd_v[storeIndexD]=d_v;
-//        storeIndexD = storeIndexD + 1;
-//
-//// picking up the delayed d_v
-//   Vector3f d_v_Delay=storedd_v[bestStoreIndex];
-//// v_hat
-//   v_hat = state.velocity + d_v- d_v_Delay;
-//
-//
-//// position prediction
-//   d_p+= v_hat*dtIMU;
-//// buffering d_p
-//        storeIndexD = storeIndexD - 1;
-//	storedd_p[storeIndexD]=d_p;
-//        storeIndexD = storeIndexD + 1;
-//// picking up the delayed d_p
-//   Vector3f d_p_Delay=storedd_p[bestStoreIndex];
-//// p_hat
-//   p_hat = state.position + d_p- d_p_Delay;
-//
-//
-//
-//// Mixed-invariant
-//
-//D_q_k1.rotation_matrix(Tbn_temp);
-//
-//d_v_m= d_v_m+Tbn_temp*corrected_tilde_Vel12 ;
-//
-//// buffering d_v_m
-//        storeIndexD = storeIndexD - 1;
-//	storedd_v_m[storeIndexD]=d_v_m;
-//        storeIndexD = storeIndexD + 1;
-//
-//
-//ctr_rst+=1;
-////printf("%u \n",ctr_rst);
-///*
-//Vector3f tmp_rst_valu;
-//tmp_rst_valu[0]=0;
-//tmp_rst_valu[1]=0;
-//tmp_rst_valu[2]=0;
-//*/
-//    if (ctr_rst==100) {   // reset every ctr_rst*20 (ms)
-//        ctr_rst=0;
-//        for (uint16_t i=0; i<=(BUFFER_SIZE-1); i++)
-//        {
-//	      storedd_v_m[i]=storedd_v_m[i]-d_v_m;
-//        }
-//	d_v_m[0]=0;
-//	d_v_m[1]=0;
-//	d_v_m[2]=0;
-//    }
-//
-//
-//// picking up the delayed d_v
-//   Vector3f d_v_m_Delay=storedd_v_m[bestStoreIndex];
-//
-//// D_q_delay^{-1}
-//q_tmp[0]= D_Delay[0];
-//q_tmp[1]= -D_Delay[1];
-//q_tmp[2]= -D_Delay[2];
-//q_tmp[3]= -D_Delay[3];
-//
-//// q_hat_tau \times D_q_delay^{-1}
-//q_tmp_m[0] = state.quat[0]*q_tmp[0] - state.quat[1]*q_tmp[1] - state.quat[2]*q_tmp[2] - state.quat[3]*q_tmp[3];
-//q_tmp_m[1] = state.quat[0]*q_tmp[1] + state.quat[1]*q_tmp[0] + state.quat[2]*q_tmp[3] - state.quat[3]*q_tmp[2];
-//q_tmp_m[2] = state.quat[0]*q_tmp[2] + state.quat[2]*q_tmp[0] + state.quat[3]*q_tmp[1] - state.quat[1]*q_tmp[3];
-//q_tmp_m[3] = state.quat[0]*q_tmp[3] + state.quat[3]*q_tmp[0] + state.quat[1]*q_tmp[2] - state.quat[2]*q_tmp[1];
-//
-//q_tmp_m.rotation_matrix(Tbn_temp);
-//// Tbn_temp = Tbn_temp.transposed();
-//
-//// v_hat_m mixed-invariant
-//v_hat_m = state.velocity + gravityNED*(0.001f*constrain_int16(_msecPosDelay, 0, MAX_MSDELAY))+Tbn_temp*(d_v_m- d_v_m_Delay);
-//
-//// position prediction
-//   d_p_m+= v_hat_m*dtIMU;
-//// buffering d_p
-//        storeIndexD = storeIndexD - 1;
-//	storedd_p_m[storeIndexD]=d_p_m;
-//        storeIndexD = storeIndexD + 1;
-//// picking up the delayed d_p
-//   d_p_Delay=storedd_p_m[bestStoreIndex];
-//// p_hat
-//   p_hat_m = state.position + d_p_m- d_p_Delay;
-//
-//
-////static FILE *mylog;
-////if (mylog==NULL){
-////	mylog = fopen("logtmp.txt", "w");
-////}
-////if (mylog!=NULL){
-////// fputs ("Ali",  mylog);
-////fprintf (mylog, "{%lu;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f}\n", hal.scheduler->millis(), q_hat[0], q_hat[1],q_hat[2],q_hat[3],v_hat[0],v_hat[1],v_hat[2],d_v[0],d_v[1],d_v[2],d_v_Delay[0],d_v_Delay[1],d_v_Delay[2],p_hat[0],p_hat[1],p_hat[2],v_hat_m[0],v_hat_m[1],v_hat_m[2],p_hat_m[0],p_hat_m[1],p_hat_m[2]);
-////}
-///*
-//if (mylog){
-//	fprintf("%f", q_hat[0]);
-//}
-//*/
-//
-//// update predictor R_hat
-///*
-//R_hat_T = R_hat;
-//R_hat[0][0] = D_T[0][0]*(D[0][0]*R_hat_T[0][0]+D[1][0]*R_hat_T[0][1]+D[2][0]*R_hat_T[0][2])+D_T[1][0]*(D[0][1]*R_hat_T[0][0]+D[1][1]*R_hat_T[0][1]+D[2][1]*R_hat_T[0][2])+D_T[2][0]*(D[0][2]*R_hat_T[0][0]+D[1][2]*R_hat_T[0][1]+D[2][2]*R_hat_T[0][2]);
-//R_hat[0][1] = D_T[0][1]*(D[0][0]*R_hat_T[0][0]+D[1][0]*R_hat_T[0][1]+D[2][0]*R_hat_T[0][2])+D_T[1][1]*(D[0][1]*R_hat_T[0][0]+D[1][1]*R_hat_T[0][1]+D[2][1]*R_hat_T[0][2])+D_T[2][1]*(D[0][2]*R_hat_T[0][0]+D[1][2]*R_hat_T[0][1]+D[2][2]*R_hat_T[0][2]);
-//R_hat[0][2] = D_T[0][2]*(D[0][0]*R_hat_T[0][0]+D[1][0]*R_hat_T[0][1]+D[2][0]*R_hat_T[0][2])+D_T[1][2]*(D[0][1]*R_hat_T[0][0]+D[1][1]*R_hat_T[0][1]+D[2][1]*R_hat_T[0][2])+D_T[2][2]*(D[0][2]*R_hat_T[0][0]+D[1][2]*R_hat_T[0][1]+D[2][2]*R_hat_T[0][2]);
-//R_hat[1][0] = D_T[0][0]*(D[0][0]*R_hat_T[1][0]+D[1][0]*R_hat_T[1][1]+D[2][0]*R_hat_T[1][2])+D_T[1][0]*(D[0][1]*R_hat_T[1][0]+D[1][1]*R_hat_T[1][1]+D[2][1]*R_hat_T[1][2])+D_T[2][0]*(D[0][2]*R_hat_T[1][0]+D[1][2]*R_hat_T[1][1]+D[2][2]*R_hat_T[1][2]);
-//R_hat[1][1] = D_T[0][1]*(D[0][0]*R_hat_T[1][0]+D[1][0]*R_hat_T[1][1]+D[2][0]*R_hat_T[1][2])+D_T[1][1]*(D[0][1]*R_hat_T[1][0]+D[1][1]*R_hat_T[1][1]+D[2][1]*R_hat_T[1][2])+D_T[2][1]*(D[0][2]*R_hat_T[1][0]+D[1][2]*R_hat_T[1][1]+D[2][2]*R_hat_T[1][2]);
-//R_hat[1][2] = D_T[0][2]*(D[0][0]*R_hat_T[1][0]+D[1][0]*R_hat_T[1][1]+D[2][0]*R_hat_T[1][2])+D_T[1][2]*(D[0][1]*R_hat_T[1][0]+D[1][1]*R_hat_T[1][1]+D[2][1]*R_hat_T[1][2])+D_T[2][2]*(D[0][2]*R_hat_T[1][0]+D[1][2]*R_hat_T[1][1]+D[2][2]*R_hat_T[1][2]);
-//R_hat[2][0] = D_T[0][0]*(D[0][0]*R_hat_T[2][0]+D[1][0]*R_hat_T[2][1]+D[2][0]*R_hat_T[2][2])+D_T[1][0]*(D[0][1]*R_hat_T[2][0]+D[1][1]*R_hat_T[2][1]+D[2][1]*R_hat_T[2][2])+D_T[2][0]*(D[0][2]*R_hat_T[2][0]+D[1][2]*R_hat_T[2][1]+D[2][2]*R_hat_T[2][2]);
-//R_hat[2][1] = D_T[0][1]*(D[0][0]*R_hat_T[2][0]+D[1][0]*R_hat_T[2][1]+D[2][0]*R_hat_T[2][2])+D_T[1][1]*(D[0][1]*R_hat_T[2][0]+D[1][1]*R_hat_T[2][1]+D[2][1]*R_hat_T[2][2])+D_T[2][1]*(D[0][2]*R_hat_T[2][0]+D[1][2]*R_hat_T[2][1]+D[2][2]*R_hat_T[2][2]);
-//R_hat[2][2] = D_T[0][2]*(D[0][0]*R_hat_T[2][0]+D[1][0]*R_hat_T[2][1]+D[2][0]*R_hat_T[2][2])+D_T[1][2]*(D[0][1]*R_hat_T[2][0]+D[1][1]*R_hat_T[2][1]+D[2][1]*R_hat_T[2][2])+D_T[2][2]*(D[0][2]*R_hat_T[2][0]+D[1][2]*R_hat_T[2][1]+D[2][2]*R_hat_T[2][2]);
-//*/
-
-///////////////////////////////////////  end sean predictor //////////////////////////////////////////////////////
 }
 
 // calculate the predicted state covariance matrix
@@ -3695,7 +3385,7 @@ if ((lastFixTime_ms != VelTimeStamp[bestStoreIndex]) && (bestTimeDelta < MAX_MSE
 void NavEKF2::readHgtData()    // modified for the delayed buffer
 {
     bool tmp_newdatastored;  // shows is new data is stored in the buffer
-    uint32_t tmp_index;
+    uint16_t tmp_index;
 
     // check to see if baro measurement has changed so we know if a new measurement has arrived
     if (_baro.get_last_update() != lastHgtMeasTime1) {
@@ -4078,30 +3768,31 @@ void NavEKF2::ZeroVariables()
 
 // Delta Predictor ///////////////
 
-    D[0][0] = 1;
-    D[0][1] = 0;
-    D[0][2] = 0;
-    D[1][0] = 0;
-    D[1][1] = 1;
-    D[1][2] = 0;
-    D[2][0] = 0;
-    D[2][1] = 0;
-    D[2][2] = 1;
-    D_T = D;
-    R_hat_T = D;
-    R_hat = D;
-    D_q[0] = 1;
-    D_q[1] = 0;
-    D_q[2] = 0;
-    D_q[3] = 0;
-    D_q_k1 = D_q;
+//    D[0][0] = 1;
+//    D[0][1] = 0;
+//    D[0][2] = 0;
+//    D[1][0] = 0;
+//    D[1][1] = 1;
+//    D[1][2] = 0;
+//    D[2][0] = 0;
+//    D[2][1] = 0;
+//    D[2][2] = 1;
+//    D_T = D;
+//    R_hat_T = D;
+//    R_hat = D;
+//    D_q[0] = 1;
+//    D_q[1] = 0;
+//    D_q[2] = 0;
+//    D_q[3] = 0;
+//    D_q_k1 = D_q;
+
 //    q_hat = D_q;
 //    q_hat_T_k1 = D_q;
-    tilde_q[0] = 1;
-    tilde_q[1] = 0;
-    tilde_q[2] = 0;
-    n_tilde_q = 1;
-    delta_q = D_q;
+ //   tilde_q[0] = 1;
+//    tilde_q[1] = 0;
+//    tilde_q[2] = 0;
+//    n_tilde_q = 1;
+//    delta_q = D_q;
 
 /////////////////////////
 
