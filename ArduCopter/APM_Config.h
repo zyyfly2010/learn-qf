6b65e85e8217d71b82b224fff1906b91a65a6654
Andrew Tridgell <andrew@tridgell.net>
2015-05-29 16:14:41 +1000

    Copter: disable terrain following for NavEKF2
    to have enough memory

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -53,3 +53,7 @@
 //#define USERHOOK_MEDIUMLOOP userhook_MediumLoop();        // for code to be run at 10hz
 //#define USERHOOK_SLOWLOOP userhook_SlowLoop();            // for code to be run at 3.3hz
 //#define USERHOOK_SUPERSLOWLOOP userhook_SuperSlowLoop();  // for code to be run at 1hz
+
+#undef AP_TERRAIN_AVAILABLE
+#define AP_TERRAIN_AVAILABLE 0
+
