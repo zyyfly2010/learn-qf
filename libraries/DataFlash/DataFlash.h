3drobotics/ardupilot-solo

DataFlash: strip extra channels from RCOU

libraries/DataFlash/DataFlash.h
@@ -264,14 +264,6 @@ struct PACKED log_RCOUT {
     uint16_t chan2;
     uint16_t chan3;
     uint16_t chan4;
-    uint16_t chan5;
-    uint16_t chan6;
-    uint16_t chan7;
-    uint16_t chan8;
-    uint16_t chan9;
-    uint16_t chan10;
-    uint16_t chan11;
-    uint16_t chan12;
 };
 
 struct PACKED log_BARO {
@@ -568,7 +560,7 @@ Format characters in the format string for binary log messages
     { LOG_RCIN_MSG, sizeof(log_RCIN), \
       "RCIN",  "Ihhhhhhhhhhhhhh",     "TimeMS,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14" }, \
     { LOG_RCOUT_MSG, sizeof(log_RCOUT), \
-      "RCOU",  "Ihhhhhhhhhhhh",     "TimeMS,Ch1,Ch2,Ch3,Ch4,Ch5,Ch6,Ch7,Ch8,Ch9,Ch10,Ch11,Ch12" }, \
+      "RCOU",  "Ihhhh",     "TimeMS,Ch1,Ch2,Ch3,Ch4" }, \
     { LOG_BARO_MSG, sizeof(log_BARO), \
       "BARO",  "Iffcf", "TimeMS,Alt,Press,Temp,CRt" }, \
     { LOG_BAR2_MSG, sizeof(log_BARO), \

zyyfly2010/learn-qf

libraries/DataFlash/DataFlash.h
@@ -301,14 +301,6 @@ struct PACKED log_RCOUT {
     uint16_t chan2;
     uint16_t chan3;
     uint16_t chan4;
-    uint16_t chan5;
-    uint16_t chan6;
-    uint16_t chan7;
-    uint16_t chan8;
-    uint16_t chan9;
-    uint16_t chan10;
-    uint16_t chan11;
-    uint16_t chan12;
 };
 
 struct PACKED log_BARO {
@@ -659,7 +651,7 @@ Format characters in the format string for binary log messages
     { LOG_RCIN_MSG, sizeof(log_RCIN), \
       "RCIN",  "Qhhhhhhhhhhhhhh",     "TimeUS,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14" }, \
     { LOG_RCOUT_MSG, sizeof(log_RCOUT), \
-      "RCOU",  "Qhhhhhhhhhhhh",     "TimeUS,Ch1,Ch2,Ch3,Ch4,Ch5,Ch6,Ch7,Ch8,Ch9,Ch10,Ch11,Ch12" }, \
+      "RCOU",  "Qhhhh",     "TimeUS,Ch1,Ch2,Ch3,Ch4" }, \
     { LOG_BARO_MSG, sizeof(log_BARO), \
       "BARO",  "Qffcf", "TimeUS,Alt,Press,Temp,CRt" }, \
     { LOG_POWR_MSG, sizeof(log_POWR), \