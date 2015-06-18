3drobotics/ardupilot-solo

DataFlash: gimbal data logging

libraries/DataFlash/DataFlash.h
@@ -10,6 +10,7 @@
 #include <AP_Param.h>
 #include <AP_GPS.h>
 #include <AP_InertialSensor.h>
+#include <AP_Gimbal.h>
 #include <AP_Baro.h>
 #include <AP_AHRS.h>
 #include "../AP_Airspeed/AP_Airspeed.h"
@@ -84,6 +85,7 @@ public:
 	void Log_Write_Current(const AP_BattMonitor &battery, int16_t throttle);
     void Log_Write_Compass(const Compass &compass);
     void Log_Write_Mode(uint8_t mode);
+    void Log_Write_Gimbal(const AP_Gimbal &gimbal);
 
     bool logging_started(void) const { return log_write_started; }
 
@@ -205,6 +207,36 @@ struct PACKED log_IMU {
     float gyro_x, gyro_y, gyro_z;
     float accel_x, accel_y, accel_z;
     uint32_t gyro_error, accel_error;
+};
+
+struct PACKED log_Gimbal1 {
+    LOG_PACKET_HEADER;
+    uint32_t time_ms;
+    float delta_time;
+    float delta_angles_x;
+    float delta_angles_y;
+    float delta_angles_z;
+    float delta_velocity_x;
+    float delta_velocity_y;
+    float delta_velocity_z;
+    float joint_angles_x;
+    float joint_angles_y;
+    float joint_angles_z;
+};
+
+struct PACKED log_Gimbal2 {
+    LOG_PACKET_HEADER;
+    uint32_t time_ms;
+    uint8_t  est_sta;
+    float est_x;
+    float est_y;  
+    float est_z;  
+    float rate_x;  
+    float rate_y; 
+    float rate_z; 
+    float target_x;
+    float target_y;
+    float target_z;
 };
 
 struct PACKED log_RCIN {
@@ -610,7 +642,11 @@ Format characters in the format string for binary log messages
     { LOG_COMPASS2_MSG, sizeof(log_Compass), \
       "MAG2","Ihhhhhhhhh",    "TimeMS,MagX,MagY,MagZ,OfsX,OfsY,OfsZ,MOfsX,MOfsY,MOfsZ" }, \
     { LOG_COMPASS3_MSG, sizeof(log_Compass), \
-      "MAG3","Ihhhhhhhhh",    "TimeMS,MagX,MagY,MagZ,OfsX,OfsY,OfsZ,MOfsX,MOfsY,MOfsZ" } \
+      "MAG3","Ihhhhhhhhh",    "TimeMS,MagX,MagY,MagZ,OfsX,OfsY,OfsZ,MOfsX,MOfsY,MOfsZ" }, \
+    { LOG_GIMBAL1_MSG, sizeof(log_Gimbal1), \
+      "GMB1", "Iffffffffff", "TimeMS,dt,dax,day,daz,dvx,dvy,dvz,jx,jy,jz" }, \
+    { LOG_GIMBAL2_MSG, sizeof(log_Gimbal2), \
+      "GMB2", "IBfffffffff", "TimeMS,es,ex,ey,ez,rx,ry,rz,tx,ty,tz" }
 
 #if HAL_CPU_CLASS >= HAL_CPU_CLASS_75
 #define LOG_COMMON_STRUCTURES LOG_BASE_STRUCTURES, LOG_EXTRA_STRUCTURES
@@ -664,6 +700,8 @@ Format characters in the format string for binary log messages
 #define LOG_COMPASS2_MSG  168
 #define LOG_COMPASS3_MSG  169
 #define LOG_MODE_MSG      170
+#define LOG_GIMBAL1_MSG   171
+#define LOG_GIMBAL2_MSG   172
 
 // message types 200 to 210 reversed for GPS driver use
 // message types 211 to 220 reversed for autotune use

libraries/DataFlash/LogFile.cpp
@@ -843,6 +843,50 @@ void DataFlash_Class::Log_Write_IMU(const AP_InertialSensor &ins)
 #endif
 }
 
+// Write a gimbal measurament and estimation data packet
+void DataFlash_Class::Log_Write_Gimbal(const AP_Gimbal &gimbal)
+{
+    uint32_t tstamp = hal.scheduler->millis();
+    Quaternion quatEst;
+    gimbal._ekf.getQuat(quatEst);
+    Vector3f eulerEst;
+    quatEst.to_euler(eulerEst.x, eulerEst.y, eulerEst.z);
+
+    struct log_Gimbal1 pkt1 = {
+        LOG_PACKET_HEADER_INIT(LOG_GIMBAL1_MSG),
+        time_ms : tstamp,
+        delta_time      : gimbal._measurament.delta_time,
+        delta_angles_x  : gimbal._measurament.delta_angles.x,
+        delta_angles_y  : gimbal._measurament.delta_angles.y,
+        delta_angles_z  : gimbal._measurament.delta_angles.z,
+        delta_velocity_x : gimbal._measurament.delta_velocity.x,
+        delta_velocity_y : gimbal._measurament.delta_velocity.y,
+        delta_velocity_z : gimbal._measurament.delta_velocity.z,
+        joint_angles_x  : gimbal._measurament.joint_angles.x,
+        joint_angles_y  : gimbal._measurament.joint_angles.y,
+        joint_angles_z  : gimbal._measurament.joint_angles.z
+    };
+    WriteBlock(&pkt1, sizeof(pkt1));
+
+    struct log_Gimbal2 pkt2 = {
+        LOG_PACKET_HEADER_INIT(LOG_GIMBAL2_MSG),
+        time_ms : tstamp,
+        est_sta : (uint8_t) gimbal._ekf.getStatus(),
+        est_x   : eulerEst.x,
+        est_y   : eulerEst.y,
+        est_z   : eulerEst.z,
+        rate_x  : gimbal.gimbalRateDemVec.x,
+        rate_y  : gimbal.gimbalRateDemVec.y,
+        rate_z  : gimbal.gimbalRateDemVec.z,
+        target_x: gimbal._angle_ef_target_rad.x,
+        target_y: gimbal._angle_ef_target_rad.y,
+        target_z: gimbal._angle_ef_target_rad.z
+       };
+    WriteBlock(&pkt2, sizeof(pkt2));
+}
+
+
+
 // Write a text message to the log
 void DataFlash_Class::Log_Write_Message(const char *message)
 {

zyyfly2010/learn-qf

libraries/DataFlash/DataFlash.h
@@ -11,6 +11,7 @@
 #include <AP_Param.h>
 #include <AP_GPS.h>
 #include <AP_InertialSensor.h>
+#include <AP_Gimbal.h>
 #include <AP_Baro.h>
 #include <AP_AHRS.h>
 #include <AP_Vehicle.h>
@@ -102,6 +103,7 @@ public:
     };
 
     void Log_Write_PID(uint8_t msg_type, const PID_Info &info);
+    void Log_Write_Gimbal(const AP_Gimbal &gimbal);
 
     bool logging_started(void) const { return log_write_started; }
 
@@ -241,6 +243,36 @@ struct PACKED log_Vibe {
     uint64_t time_us;
     float vibe_x, vibe_y, vibe_z;
     uint32_t clipping_0, clipping_1, clipping_2;
+};
+
+struct PACKED log_Gimbal1 {
+    LOG_PACKET_HEADER;
+    uint64_t time_us;
+    float delta_time;
+    float delta_angles_x;
+    float delta_angles_y;
+    float delta_angles_z;
+    float delta_velocity_x;
+    float delta_velocity_y;
+    float delta_velocity_z;
+    float joint_angles_x;
+    float joint_angles_y;
+    float joint_angles_z;
+};
+
+struct PACKED log_Gimbal2 {
+    LOG_PACKET_HEADER;
+    uint64_t time_us;
+    uint8_t  est_sta;
+    float est_x;
+    float est_y;  
+    float est_z;  
+    float rate_x;  
+    float rate_y; 
+    float rate_z; 
+    float target_x;
+    float target_y;
+    float target_z;
 };
 
 struct PACKED log_RCIN {
@@ -732,7 +764,11 @@ Format characters in the format string for binary log messages
     { LOG_IMUDT2_MSG, sizeof(log_IMUDT), \
       "IMT2","Qffffffff","TimeUS,DelT,DelvT,DelAX,DelAY,DelAZ,DelVX,DelVY,DelVZ" }, \
     { LOG_IMUDT3_MSG, sizeof(log_IMUDT), \
-      "IMT3","Qffffffff","TimeUS,DelT,DelvT,DelAX,DelAY,DelAZ,DelVX,DelVY,DelVZ" }
+      "IMT3","Qffffffff","TimeUS,DelT,DelvT,DelAX,DelAY,DelAZ,DelVX,DelVY,DelVZ" }, \
+    { LOG_GIMBAL1_MSG, sizeof(log_Gimbal1), \
+      "GMB1", "Qffffffffff", "TimeUS,dt,dax,day,daz,dvx,dvy,dvz,jx,jy,jz" }, \
+    { LOG_GIMBAL2_MSG, sizeof(log_Gimbal2), \
+      "GMB2", "QBfffffffff", "TimeUS,es,ex,ey,ez,rx,ry,rz,tx,ty,tz" }
 
 #if HAL_CPU_CLASS >= HAL_CPU_CLASS_75
 #define LOG_COMMON_STRUCTURES LOG_BASE_STRUCTURES, LOG_EXTRA_STRUCTURES
@@ -802,6 +838,8 @@ Format characters in the format string for binary log messages
 #define LOG_IMUDT_MSG     184
 #define LOG_IMUDT2_MSG    185
 #define LOG_IMUDT3_MSG    186
+#define LOG_GIMBAL1_MSG   187
+#define LOG_GIMBAL2_MSG   188
 
 // message types 200 to 210 reversed for GPS driver use
 // message types 211 to 220 reversed for autotune use

libraries/DataFlash/LogFile.cpp
@@ -961,6 +961,48 @@ void DataFlash_Class::Log_Write_Vibration(const AP_InertialSensor &ins)
 #endif
 }
 
+// Write a gimbal measurament and estimation data packet
+void DataFlash_Class::Log_Write_Gimbal(const AP_Gimbal &gimbal)
+{
+    uint64_t tstamp = hal.scheduler->micros64();
+    Quaternion quatEst;
+    gimbal._ekf.getQuat(quatEst);
+    Vector3f eulerEst;
+    quatEst.to_euler(eulerEst.x, eulerEst.y, eulerEst.z);
+
+    struct log_Gimbal1 pkt1 = {
+        LOG_PACKET_HEADER_INIT(LOG_GIMBAL1_MSG),
+        time_us         : tstamp,
+        delta_time      : gimbal._measurament.delta_time,
+        delta_angles_x  : gimbal._measurament.delta_angles.x,
+        delta_angles_y  : gimbal._measurament.delta_angles.y,
+        delta_angles_z  : gimbal._measurament.delta_angles.z,
+        delta_velocity_x : gimbal._measurament.delta_velocity.x,
+        delta_velocity_y : gimbal._measurament.delta_velocity.y,
+        delta_velocity_z : gimbal._measurament.delta_velocity.z,
+        joint_angles_x  : gimbal._measurament.joint_angles.x,
+        joint_angles_y  : gimbal._measurament.joint_angles.y,
+        joint_angles_z  : gimbal._measurament.joint_angles.z
+    };
+    WriteBlock(&pkt1, sizeof(pkt1));
+
+    struct log_Gimbal2 pkt2 = {
+        LOG_PACKET_HEADER_INIT(LOG_GIMBAL2_MSG),
+        time_us : tstamp,
+        est_sta : (uint8_t) gimbal._ekf.getStatus(),
+        est_x   : eulerEst.x,
+        est_y   : eulerEst.y,
+        est_z   : eulerEst.z,
+        rate_x  : gimbal.gimbalRateDemVec.x,
+        rate_y  : gimbal.gimbalRateDemVec.y,
+        rate_z  : gimbal.gimbalRateDemVec.z,
+        target_x: gimbal._angle_ef_target_rad.x,
+        target_y: gimbal._angle_ef_target_rad.y,
+        target_z: gimbal._angle_ef_target_rad.z
+       };
+    WriteBlock(&pkt2, sizeof(pkt2));
+}
+
 // Write a text message to the log
 void DataFlash_Class::Log_Write_Message(const char *message)
 {