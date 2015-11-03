94c9c64bc2e77e6491483f041dedbaacd32bdc66
Tom Pittenger <magicrub@gmail.com>
2015-11-02 23:29:40 -0800

    Plane: fixed regression, restore packet handling
    - accidently removed during the great send_vehicle purge

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -1902,6 +1902,10 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
                                 (uint32_t)(new_home_loc.alt*0.01f));
         break;
     }
+
+    case MAVLINK_MSG_ID_ADSB_VEHICLE:
+        plane.adsb.update_vehicle(msg);
+        break;
     } // end switch
 } // end handle mavlink
 
