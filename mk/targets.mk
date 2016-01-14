c5712c5caee296652726f13d9b62b27bbf1610d1
Andrew Tridgell <andrew@tridgell.net>
2016-01-15 08:21:42 +1100

    mk: fixed building with mavlink2 headers

--- mk/targets.mk
+++ mk/targets.mk
@@ -91,9 +91,6 @@ pxfmini: all
 # support debug build
 %-debug: OPTFLAGS = -g -O0
 
-# support mavlink2 builds
-%-mavlink2: EXTRAFLAGS += "-DMAVLINK_PROTOCOL_VERSION=2"
-
 # support address sanitiser
 %-asan: OPTFLAGS = -g -O0 -fsanitize=address -fno-omit-frame-pointer
 %-asan: LDFLAGS += -fsanitize=address
